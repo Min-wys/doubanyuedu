ab81c2d9fa1969d2088f7a0e08fc317c19d316a5
daiwenwen399 <944148423@qq.com>
2021-01-04 21:33:01 +0800

    feat:完善静态页面

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
-import { Button, Breadcrumb, BreadcrumbItem } from 'element-ui'
+import { Button, Breadcrumb, BreadcrumbItem, Pagination } from 'element-ui'
 
 Vue.use(Button)
 Vue.use(Breadcrumb)
 Vue.use(BreadcrumbItem)
+Vue.use(Pagination)
--- src/views/category/index.vue
+++ src/views/category/index.vue
@@ -15,14 +15,12 @@
       <div class="nav">
         <div class="nav_main">
           <div class="nav_select">
-            <a class="select_control cursorsHover"
-              >分类<i class="el-icon-arrow-down"></i
-            ></a>
+            <a class="select_control">分类<i class="el-icon-arrow-down"></i></a>
             <a class="select_control">筛选<i class="el-icon-arrow-down"></i></a>
           </div>
           <div class="nav_tags"></div>
           <div class="nav_sort">
-            <a class="sort_option selected cursorsHover">热度</a>
+            <a class="sort_option selected">热度</a>
             <a class="sort_option">加入书架数</a>
             <a class="sort_option">更新时间</a>
             <a class="sort_option">销量从高到低</a>
@@ -30,7 +28,7 @@
           </div>
         </div>
         <div class="classification">
-          <button class="btn cursorsHover">全部</button>
+          <button class="btn hover">全部</button>
           <button class="btn">全部</button>
           <button class="btn">全部</button>
           <button class="btn">全部</button>
@@ -128,6 +126,8 @@
       </div>
 
       <!-- 分页器 -->
+      <el-pagination background layout="prev, pager, next" :total="1000">
+      </el-pagination>
     </div>
   </div>
 </template>
@@ -135,6 +135,15 @@
 <script>
 export default {
   name: "Category",
+  data() {
+    return {
+      isHover: true,
+      isSelected: true,
+    };
+  },
+  methods: {
+    addHover() {},
+  },
 };
 </script>
 
@@ -144,7 +153,7 @@ export default {
   height: 100%;
   background-color: #f8f9f9;
   overflow: hidden;
-  .cursorsHover {
+  .hover {
     color: #fff;
     background-color: #389eac !important;
   }
@@ -178,6 +187,10 @@ export default {
       padding-left: 15px;
       cursor: pointer;
     }
+    .select_control:hover {
+      color: #fff;
+      background-color: #389eac;
+    }
     .el-icon-arrow-down {
       padding-left: 15px;
     }
@@ -191,6 +204,10 @@ export default {
       cursor: pointer;
     }
     .sort_option.selected {
+      color: #fff;
+      background-color: #389eac;
+    }
+    .sort_option:hover {
       color: #fff;
       background-color: #389eac;
     }
@@ -209,6 +226,11 @@ export default {
       box-shadow: 1px 1px 3px 0 rgba(0, 0, 0, 0.2);
       border: none;
       cursor: pointer;
+      outline: none;
+      &:hover {
+        color: #fff;
+        background-color: #389eac;
+      }
     }
   }
   // 主要内容区
@@ -237,14 +259,21 @@ export default {
     }
     .info {
       h4 {
+        color: #333;
         font-size: 16px;
         font-weight: bold;
         margin-bottom: 5px;
+        &:hover {
+          color: #000;
+        }
       }
       .author {
         font-size: 12px;
         margin-bottom: 12px;
         color: #777;
+        &:hover {
+          color: #389eac;
+        }
       }
       .intro {
         font-size: 13px;
@@ -259,6 +288,7 @@ export default {
     }
     .extra_info {
       margin-bottom: 8px;
+      color: #777;
       .separator {
         width: 1px;
         height: 10px;
@@ -267,7 +297,8 @@ export default {
         display: inline-block;
       }
       a:hover {
-        color: #389eac !important;
+        color: #389eac;
+        text-decoration: none;
       }
     }
     .addBookshelf {
@@ -280,7 +311,17 @@ export default {
       box-shadow: 1px 1px 3px 0 rgba(0, 0, 0, 0.2);
       border: none;
       cursor: pointer;
+      outline: none;
+      &:hover {
+        color: #fff;
+        background-color: #389eac;
+      }
     }
+  }
+  // 分页器区
+  .el-pagination {
+    margin: 30px 0;
+    text-align: center;
   }
 }
 </style>
